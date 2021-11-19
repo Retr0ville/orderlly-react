c4b89f7a4643b88be92c1a0af53985b00ffef0c3
Rtrvl <xclusivesam14@gmail.com>
2021-11-19 05:16:03 +0100

    use one list component

---
+++ client/src/pages/itemList/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../layout/header'
+import Footer from '../../layout/footer'
+import Content from '../../layout/content'
+
+const itemList = ({url}) => {
+  // const endpoint = url === "/pcs"? "" : url === "/pc-accessories"? "" : url === "/smartphones"? "" : url === "/smartphone-accessories"? "" : ""
+  return (
+    <div>
+      <Header />
+      <div className="main">
+        <Content
+        endpoint = {url} />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default itemList
