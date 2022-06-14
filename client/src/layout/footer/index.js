71ce9051267af72de0ea257bee7ce647702e1902
Retr0ville <xclusivesam14@gmail.com>
2022-06-14 21:44:13 +0100

    footer update

--- client/src/layout/footer/index.js
+++ client/src/layout/footer/index.js
@@ -3,21 +3,21 @@ import React from "react";
 const Footer = (props) => {
   return (
     <div className="footer h-100 bg-dark">
-      <div className="footer-nav d-flex justify-content-end">
+      <div className="footer-nav d-flex justify-content-center justify-content-md-end">
         <nav>
-          <ul className="nav flex-column mt-4 w-100">
+          <ul className="nav flex-column mt-4 w-100 text-center text-md-end mb-3 mb-md-0">
             <li className="nav-item mb-3 ">
-              <a className="nav-link text-secondary" href="/">
-                <i class="fa fa-github " aria-hidden="true"></i> Rtrvl's Github
+              <a className="nav-link text-secondary " href="https://github.com/Retr0ville/orderlly-react">
+                <i class="fa fa-github " aria-hidden="true"></i> My Github
               </a>
             </li>
             <li className="nav-item mb-3">
-              <a className="nav-link w-100 text-secondary" href="/">
+              <a className="nav-link w-100 text-secondary" href="https://github.com/Retr0ville">
                 <i class="fa fa-info-circle" aria-hidden="true"></i> About me
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link w-100 text-secondary" href="/">
+              <a className="nav-link w-100 text-secondary" href="https://github.com/Retr0ville">
                 <i class="fa fa-address-book" aria-hidden="true"></i> Contact
               </a>
             </li>
@@ -25,7 +25,7 @@ const Footer = (props) => {
         </nav>
       </div>
       <footer id="footer">
-        <span className=" text-black opacity-50">
+        <span className="text-secondary opacity-50">
           <i className="fa fa-copyright" aria-hidden="true"></i> Rtrvl 2022
         </span>
       </footer>
