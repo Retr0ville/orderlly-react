0e17e942d560d2b201401c921c302813a9cdd70a
Retr0ville <xclusivesam14@gmail.com>
2022-06-15 12:42:03 +0100

    add enviroment variables

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
@@ -11,8 +12,7 @@ const cookieParser = require('cookie-parser')
 
 // init
 const app = express()
-const dbUri =
-  'mongodb://Rtrvl:Sesameex1@cluster0-shard-00-00.zvqqc.mongodb.net:27017,cluster0-shard-00-01.zvqqc.mongodb.net:27017,cluster0-shard-00-02.zvqqc.mongodb.net:27017/Orderlly?ssl=true&replicaSet=atlas-itdc3i-shard-0&authSource=admin&retryWrites=true&w=majority'
+const dbUri = process.env.MONGODB_URI
 
 // app.listen(5100, (err) => {
 //   if (err) return console.log(err)
